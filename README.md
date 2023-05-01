de44f2f2833c0dc6ab644c222e57145eca3011e6
Saurav Sah <114904905+SauravSahPS@users.noreply.github.com>
2023-05-01 23:14:05 +0530

    Complete Backend code setup

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ## create a template using vite
 
 - npm create vite@latest
+
+## To fetch the data from a server or API we can use below methods. For this repo we will use axios
+
+- fetch()
+- axios - npm i axios@1.3.4
+
+## use this for fakce web api response
+
+- https://jsonplaceholder.typicode.com/
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios, { AxiosError } from "axios";
+import React, { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface User {
+  id: number;
+  name: string;
 }
 
-export default App
+const App = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  const [error, setError] = useState("");
+
+  // useEffect(() => {
+  //   axios
+  //     .get<User[]>("https://jsonplaceholder.typicode.com/users")
+  //     .then((response) => setUsers(response.data))
+  //     .catch((err) => setError(err.message));
+  // }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get<User[]>(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+
+        setUsers(res.data);
+      } catch (err) {
+        setError((err as AxiosError).message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
